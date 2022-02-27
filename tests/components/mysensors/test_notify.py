38cf6361b7828555309f81c6853536c7a021473b
Martin Hjelmare <marhje52@gmail.com>
2022-02-27 12:33:21 +0100

    Test notify discovery

--- tests/components/mysensors/test_notify.py
+++ tests/components/mysensors/test_notify.py
@@ -1,6 +1,7 @@
 """Provide tests for mysensors notify platform."""
 from __future__ import annotations
 
+from collections.abc import Callable
 from unittest.mock import MagicMock, call
 
 from mysensors.sensor import Sensor
@@ -34,3 +35,38 @@ async def test_text_type(
 
     assert transport_write.call_count == 2
     assert transport_write.call_args == call("1;1;1;0;47;Hello\n")
+
+
+async def test_text_type_discovery(
+    hass: HomeAssistant,
+    text_node: Sensor,
+    transport_write: MagicMock,
+    receive_message: Callable[[str], None],
+) -> None:
+    """Test text type discovery."""
+    receive_message("1;2;0;0;36;\n")
+    receive_message("1;2;1;0;47;test\n")
+    receive_message("1;2;1;0;47;test2\n")  # Test that more than one set message works.
+    await hass.async_block_till_done()
+
+    await hass.services.async_call(
+        NOTIFY_DOMAIN,
+        "mysensors",
+        {"message": "Hello", "target": "Text Node 1 2"},
+        blocking=True,
+    )
+
+    assert transport_write.call_count == 1
+    assert transport_write.call_args == call("1;2;1;0;47;Hello\n")
+
+    transport_write.reset_mock()
+
+    await hass.services.async_call(
+        NOTIFY_DOMAIN, "mysensors", {"message": "Hello World"}, blocking=True
+    )
+
+    assert transport_write.call_count == 2
+    assert transport_write.call_args_list == [
+        call("1;1;1;0;47;Hello World\n"),
+        call("1;2;1;0;47;Hello World\n"),
+    ]
